1766ad9419838b929c39da894575be5f5c254e37
mohammadmac <m.m.mir83hc@gmail.com>
2021-07-16 21:52:26 +0430

    progress bar added searching for width and height at same time added and some other shits

--- download.py
+++ download.py
@@ -1,33 +1,23 @@
-import os
-from re import S
 from time import sleep
-
-import requests
+from requests import get
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from re import findall
+from progress.bar import IncrementalBar
 
-driver = webdriver.Chrome()
-find = driver.find_element_by_xpath
+def browser():
+    global driver, find
+    driver = webdriver.Chrome()
+    find = driver.find_element_by_xpath
 
-def Search(): 
-    global search_word
-    search_word = input('Enter word: ')
-    search_word_url = search_word.replace(' ', '+')
-    url = 'https://duckduckgo.com/?q={}&t=h_&iar=images&iaf=size%3AWallpaper&iax=images&ia=images'.format(search_word_url)
-    driver.get(url)
-    print('\nSearching for * %s *' % (search_word))
+def search(data):
+    browser()
+    print('\nSearching for * %s *' % (data['search word']))
+    driver.get(data['url'])
     sleep(2)
-    Make_folder()
-    
-def Make_folder():
-    global folder_path
-    folder_name = ('\\' + search_word)
-    folder_path = str(os.path.dirname(os.path.realpath(__file__))) + '\\downloads' + folder_name
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    Scroll()
+    scroll()
+    check_to_download(data)
 
-def Scroll():
+def scroll():
     lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     match=False
     while(match==False):
@@ -37,155 +27,122 @@ def Scroll():
         if lastCount==lenOfPage:
             match=True
     sleep(10)
-    Select_to_download()
 
-def Select_to_download():
-    global act
-    print('\nDownload: ')
-    print('1 - Wider pictures')
-    print('2 - Higher pictures')
-    print('3 - Both')
-    print('0 - exit')
-    act = input('--> ')
-    
-    if act == '1':
-        Width()
-    elif act == '2':
-        Height()
-    elif act == '3':
-        Width()
-        Height()
-    elif act == '0':
-        pass
-    else:
-        print('Input is incorrect. Try again. ')
-        Again()
+def check_to_download(data):
+    if data['width'] and data['height'] != '':
+        both(data)
+    elif data['height'] != '':
+        height(data)
+    elif data['width'] != '':
+        width(data)
 
-def Width():
-    print('\n-------------------------------------------------------')
-    print('\n      ...::: Width :::...')
-    try:
-        count = 1 
-        while True:
-            resolution_xpath = '//*[@id="zci-images"]/div/div[2]/div/div[%s]/div[3]' % count
-            resolution = find(resolution_xpath).text
-            width_1 = width(resolution)
-            count += 1
-            if width_1 >= 5000:
-                print('\n..........%s..........' % (count - 1))
-                pic_xpath = '//*[@id="zci-images"]/div/div[2]/div/div[%s]' % (count - 1)
-                find(pic_xpath).click()
-                sleep(2)
-                pic_url = find('//*[@id="zci-images"]/div[2]/div/div[1]/div[2]/div/div[2]/div/div/a').get_attribute('href')
-                print('Resolution: ' + resolution)
-                print('link: ' + pic_url)
-                print('Downloading:')
-                file_name = filename(pic_url)
-                try:
-                    download(pic_url, file_name)
-                    print('\n' + file_name + ' downloaded')
-                except:
-                    print('\nCannot download!')
-                    driver.execute_script("window.open('%s','_blank')" % pic_url)
-                    sleep(2)
-                    driver.switch_to_window(driver.window_handles[0])
-                    pass
-    except:
-        if act == '1':
-            print('\nDone! \n')
-            Again()
-        else:
-            pass
-
-def width(resolution):
-    count = 0
-    for x in resolution:
-        if x == '×':
-            width_1 = resolution[:count]
-        count += 1
-    width_1 = int(width_1)
-    return width_1
-
-def Height():
-    print('\n-------------------------------------------------------')
-    print('\n      ...::: Height :::...')
+def both(data):
     try:
         count = 1
+        found = 1
         while True:
             resolution_xpath = '//*[@id="zci-images"]/div/div[2]/div/div[%s]/div[3]' % count
             resolution = find(resolution_xpath).text
-            height_1 = height(resolution)
+            
+            pictre_width = int(resolution.split(' × ')[0])
+            pictre_height = int(resolution.split(' × ')[1])
+
             count += 1
-            if height_1 >= 5000:
-                print('\n..........%s..........' % (count - 1))
+            if pictre_width >= int(data['width']) or pictre_height >= int(data['height']):
+                print('\n%s- %s' % (found, resolution))
                 pic_xpath = '//*[@id="zci-images"]/div[1]/div[2]/div/div[%s]/div[1]' % (count - 1)
+                #driver.execute_script("arguments[0].scrollIntoView();", find(pic_xpath))
                 find(pic_xpath).click()
+                
                 sleep(2)
                 pic_url = find('//*[@id="zci-images"]/div[2]/div/div[1]/div[2]/div/div[2]/div/div/a').get_attribute('href')
-                print('Resolution: ' + resolution)
-                print('link: ' + pic_url)
-                print('Downloading:')
-                file_name = filename(pic_url)
-                try:
-                    download(pic_url, file_name)
-                    print('\n' + file_name + ' downloaded')
-                except:
-                    print('\nCannot download!')
-                    driver.execute_script("window.open('%s','_blank')" % pic_url)
-                    sleep(2)
-                    driver.switch_to_window(driver.window_handles[0])
-                    pass
+                download(pic_url, data['save path'])
+                
+                found += 1
     except:
         print('\nDone! \n')
-        Again()
 
-def height(resolution):
-    count = 0
-    for x in resolution:
-        if x == '×':
-            height_1 = resolution[count+1:]
-        count += 1
-    height_1 = int(height_1)
-    return height_1
+def width(data):
+    try:
+        count = 1
+        found = 1
+        while True:
+            resolution_xpath = '//*[@id="zci-images"]/div/div[2]/div/div[%s]/div[3]' % count
+            resolution = find(resolution_xpath).text
+            
+            pictre_width = int(resolution.split(' × ')[0])
+            
+            count += 1
+            if pictre_width >= int(data['width']):
+                print('\n%s- %s' % (found, resolution))
+                pic_xpath = '//*[@id="zci-images"]/div[1]/div[2]/div/div[%s]/div[1]' % (count - 1)
+                find(pic_xpath).click()
+                
+                sleep(2)
+                pic_url = find('//*[@id="zci-images"]/div[2]/div/div[1]/div[2]/div/div[2]/div/div/a').get_attribute('href')
+                download(pic_url, data['save path'])
+                
+                found += 1
 
-def filename(pic_url):
-    count = 1
-    revers_url = pic_url[::-1]
-    for i in revers_url:
-        if i == '/':
-            name = revers_url[:count-1]
-            file_name = name[::-1]
-            if file_name[-4] != '.':
-                file_name = file_name + '.jpg'
-            print(file_name)
-            break
-        count += 1
-    return file_name
+    except:
+        print('\nDone! \n')
+        
+def height(data):
+    try:
+        count = 1
+        found = 1
+        while True:
+            resolution_xpath = '//*[@id="zci-images"]/div/div[2]/div/div[%s]/div[3]' % count
+            resolution = find(resolution_xpath).text
+            
+            pictre_height = int(resolution.split(' × ')[1])
+            
+            count += 1
+            if pictre_height >= int(data['height']):
+                print('\n%s- %s' % (found, resolution))
+                pic_xpath = '//*[@id="zci-images"]/div[1]/div[2]/div/div[%s]/div[1]' % (count - 1)
+                find(pic_xpath).click()
+                
+                sleep(2)
+                pic_url = find('//*[@id="zci-images"]/div[2]/div/div[1]/div[2]/div/div[2]/div/div/a').get_attribute('href')
+                download(pic_url, data['save path'])
 
-def download(pic_url, file_name):
-    save_path = folder_path + '\\' + file_name
-    with open(save_path, 'wb') as outfile:
-        r = requests.get(pic_url)
-        outfile.write(r.content)
+                found += 1
+                
+    except:
+        print('\nDone! \n')
 
-def Again():
-    print('Do you want search again?')
-    print('1 - Search')
-    print('2 - search for current searched title')
-    print('0 - Exit')
-    act = input('--> ')
-    if act.lower() in ['1', 'search']:
-        Search()
-    elif act == '2':
-        Select_to_download()
-    elif act.lower() in ['0', 'exit']:
-        exit_func()    
-    else:
-        print('Input is incorrect. Try again. ')
-        Again()
+def download(pic_url, save_path):
+    try:
+        r = get(pic_url, stream=True)
+
+        if "Content-Disposition" in r.headers.keys():
+                file_name = findall("filename=(.+)", r.headers["Content-Disposition"])[0]
+        else:
+            file_name = pic_url.split("/")[-1]
+            
+        save_path = save_path + '/' + file_name
+
+        if r.status_code == 200:
+            total_size = int(r.headers.get('content-length', 0))
+            with open(save_path, 'wb') as file, IncrementalBar('Downloading {}'.format(file_name), max=100, suffix = '%(percent).1f%% - {}MB'.format(format(total_size/1048576, '.1f'))) as bar:
+                for data in r.iter_content(chunk_size=1024):
+                    outputfile = file.write(data)
+                    bar.next((int(outputfile) / total_size) * 100)
+        else:
+            print('Cannot download %s!' % (file_name))
+            print('So it will open in another tab to download that yourself')
+            driver.execute_script("window.open('%s','_blank')" % pic_url)
+            sleep(2)
+            driver.switch_to_window(driver.window_handles[0])
+
+    except:
+        print('Cannot download %s!' % (file_name))
+        driver.execute_script("window.open('%s','_blank')" % pic_url)
+        print('So it will open in another tab to download that yourself')
+        sleep(2)
+        driver.switch_to_window(driver.window_handles[0])
 
 def exit_func():
     print('exiting program...')
     exit()
-
-Search()
